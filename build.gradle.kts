926cf6e378fe0d35fdc3e26236f9c0a1ec691cd6
Juan Camilo Sandoval Devia <juansandoval222@gmail.com>
2023-11-27 15:54:15 -0500

    first initial setup for multitracking multimodule course
    - Kotlin DSL
    - Modules: core, coreUi, tracker (data, domain, presentation), onBoarding ( domain, presentation), buildSrc
    - Add all the dependencies in the buildSrc
    - Add the first approach to welcome screen
    - Add the compose-base and base gradle files

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,15 +5,16 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.1.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.20")
-
+        classpath(Build.androidBuildTools)
+        classpath(Build.kotlinGradlePlugin)
+        classpath(Build.hiltAndroidGradlePlugin)
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
 }
 
-tasks.register("clean", Delete::class){
+tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
 
